16d97988f441ae7b92c7e31adb1e6129839ac5ba
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-04-27 19:25:04 +0600

    post art setup done

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.5chsr9x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get('/', (req, res) => {
   res.send('Rongmohol server is running!');
 });
+
+async function run() {
+  try {
+    const database = client.db('artDB');
+    const artcollection = database.collection('artCollection');
+
+    app.post('/arts', async (req, res) => {
+      const doc = req.body;
+      const result = await artcollection.insertOne(doc);
+      res.send(result);
+    });
+
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Rongmohol server is listening on port ${port}`);
